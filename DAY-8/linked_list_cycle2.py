e7c8b12d85c1fa23595fd1ce7cbc40328a15fc23
CodeWithNeha <codewithneha2020@gmail.com>
2026-02-17 23:44:25 +0530

    Day - 8

---
+++ DAY-8/linked_list_cycle2.py
@@ -0,0 +1,37 @@
+class LinkedList:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def create_linked_list(arr):
+    dummy = LinkedList()
+    curr = dummy 
+
+    for val in arr:
+        curr.next = LinkedList(val)
+        curr = curr.next
+
+    return dummy.next
+
+head = create_linked_list([1,2,3,4,5])
+
+def detectCycle(head):
+    if not head or not head.next:
+        return None
+    slow=fast=head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return None
+    slow= head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    return slow
+    
+
+print(detectCycle(head))
