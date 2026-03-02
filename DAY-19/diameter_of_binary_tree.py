f3f341ed0c0b24001aa6b769a8e74a909e3eb2dd
CodeWithNeha <codewithneha2020@gmail.com>
2026-03-02 23:08:32 +0530

    Day - 19

---
+++ DAY-19/diameter_of_binary_tree.py
@@ -0,0 +1,17 @@
+def diameterOfBinaryTree(root):
+    diameter = 0
+
+    def height(node):
+        nonlocal diameter
+        if not node:
+            return 0
+
+        left = height(node.left)
+        right = height(node.right)
+
+        diameter = max(diameter, left + right)
+
+        return 1 + max(left, right)
+
+    height(root)
+    return diameter
